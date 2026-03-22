9079239111ac9b187c1d88120184f54ac56ce7e4
masterreps222-png <masterreps222@gmail.com>
2026-03-22 20:18:39 +0100

    Update products.js

--- products.js
+++ products.js
@@ -10,7 +10,7 @@ const productsData = [
         link: "https://ikako.vip/8qd7e",
         image: "https://si.geilicdn.com/open1731801003-1731801003-38d40000019c31209d620a8133b0_1920_1920.jpg?w=400&h=400"
     },
-{ 
+    { 
         id: 2 , 
         name: " Yeazy 350 boost", 
         categoryId: "buty", 
@@ -20,5 +20,26 @@ const productsData = [
         link: "https://ikako.vip/xp7w3",
         image: "https://si.geilicdn.com/wdseller1845163825-6498000001911f8bc1fd0a2304aa_1080_1080.jpg?w=400&h=400"
     },
+    {
+        id: 4,
+        name:"Air Max 95",
+        categoryId:"buty" , 
+        cnyPrice: 150 ,
+        rating:7.5 , 
+        tags:["Buty","PE Batch", "Budget"],
+        link:"https://ikako.vip/w52xzt",
+        image:"https://si.geilicdn.com/wdseller1845163825-563300000198f83abd420a22d58f_1179_1179.jpg"
+    },
+   {
+        id: 5,
+        name:" Bagy dresy nike ",
+        categoryId:"spodnie" , 
+        cnyPrice: 40,
+        rating: 9, 
+        tags:["Spodnie", "Bagy " , " Dresy" , "Budget" , ],
+        link:"https://www.kakobuy.com/item/details?url=https%3A%2F%2Fdetail.1688.com%2Foffer%2F978152783054.html&affcode=master15",
+        image:"https://cbu01.alicdn.com/img/ibank/O1CN01BfWkD12GgfVFZqWP3_!!2218626059045-0-cib.jpg"
+    } 
+
 ];
 
